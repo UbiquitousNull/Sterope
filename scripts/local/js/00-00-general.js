f68550a1b6a3bff8d5d8facd94b643dbc7017829
Jonathan Merrill <99621514+UbiquitousNull@users.noreply.github.com>
2024-08-29 09:27:19 +1200

    added `console.log()` wrapper.

--- scripts/local/js/00-00-general.js
+++ scripts/local/js/00-00-general.js
@@ -1,4 +1,9 @@
+function cLog(...a) {
+    console.log(...a);
+}
+
 
 function toggleActive(elementId) {
+    cLog("ToggleID: ", elementId);
     document.getElementById(elementId).classList.toggle("active");
 }
