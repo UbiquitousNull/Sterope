10cff6e563e85cdabea6442e562af64d8205cdb5
Jonathan Merrill <99621514+UbiquitousNull@users.noreply.github.com>
2024-08-29 12:18:36 +1200

    Tidying

--- scripts/local/js/00-00-general.js
+++ scripts/local/js/00-00-general.js
@@ -10,12 +10,9 @@ import {
 function toggleActive(elementId) {
     try {
         const element = document.getElementById(elementId);
-        if (!element) {
-            throw new Error(`Element with ID "${elementId}" not found.`);
-        }
+        if (!element) { throw new Error(`Element with ID "${elementId}" not found.`); }
         const result = element.classList.toggle("active");
         cLog(log, `The active class was ${result ? "added to" : "removed from"} element with ID: ${elementId}`);
-    } catch (err) {
-        cLog(error, `Error toggling active class on element with ID: ${elementId}`, err.message);
-    }
+
+    } catch (err) { cLog(error, `Error toggling active class on element with ID: ${elementId}`, err.message); }
 }
