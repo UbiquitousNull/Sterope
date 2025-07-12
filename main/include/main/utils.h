5fd73cf0959ba6e6545985ab2cbe077a88a99b96
Jonathan <99621514+UbiquitousNull@users.noreply.github.com>
2025-07-12 14:33:24 +1200

    Refactor MSGBOX_ERR macro for improved error handling; add placeholder for Unix implementation

--- main/include/main/utils.h
+++ main/include/main/utils.h
@@ -1,9 +1,20 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "Logging.h"
+#include "io.h"
+#include "main.h"
+
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 #define RETURN_ERR(...) { fprintf(stderr, __VA_ARGS__); exit(); } // Might become deprecated, use MSGBOX_ERR instead
-#define MSGBOX_ERR(fmt, ...) do { \
+
+#ifdef _WIN32
+	#include <windows.h>
+	#include <winuser.h>
+
+	// This macro currently does not have a unix equivalent, as it is used for Windows message boxes.
+	// I plan to implement a similar function for Unix systems in the future. (relevant notes on line 31)
+	#define MSGBOX_ERR(fmt, ...) do { \
     int needed = snprintf(NULL, 0, "%s:%d: " fmt, __FILE__, __LINE__, ##__VA_ARGS__); \
     if (needed > 0) { \
         char* buf = (char*)malloc(needed + 1); \
@@ -15,13 +26,11 @@
     } \
 	shutdownTotal(); \
 } while (0)
-
-#include "Logging.h"
-#include "io.h"
-#include "main.h"
-#ifdef _WIN32
-	#include <windows.h>
-	#include <winuser.h>
+#elif if __unix__ || __APPLE__
+	#define MSGBOX_ERR(fmt, ...) do { \
+	/* implement a unix version of this macro later */ \
+	shutdownTotal(); \
+} while (0)
 #endif
 
 #endif // UTILS_H
